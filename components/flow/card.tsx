9626a4867c186a7b1c6dfdc4adcc30921741d443
KazooTTT <yjwang.echo@gmail.com>
2023-06-14 23:29:53 +0800

    fix:修复文本类型Card滚动问题

--- components/flow/card.tsx
+++ components/flow/card.tsx
@@ -9,17 +9,33 @@ import { CardProps } from "@/types/flow"
 import { Skeleton } from "../ui/skeleton"
 
 function TextCard({ title, subTitle, content }: CardProps) {
+  //  计算一下content的高度，content = header - 头像 - 间距-播放器- footer
+  const size = useSize(document.body)
+  const maxHeight =
+    (size?.height ?? document.body.clientHeight) -
+    56 -
+    80 -
+    24 -
+    32 -
+    26 -
+    8 -
+    64
   return (
     <>
       <div className="mt-2 text-center font-bold">{title}</div>
       <div className="mt-0.5 text-center text-muted-foreground">{subTitle}</div>
-      <div className="mt-2 text-muted-foreground">{content}</div>
+      <div
+        className="mt-2 text-muted-foreground"
+        style={{ overflowY: "auto", maxHeight: maxHeight }}
+      >
+        {content}
+      </div>
     </>
   )
 }
 
 function ImgCard({ title, subTitle, cover, content }: CardProps) {
-  // TODO 计算一下content的高度，content = header - 头像 - 间距-播放器- footer
+  //  计算一下content的高度，content = header - 头像 - 间距-播放器- footer
   const size = useSize(document.body)
   const maxHeight =
     (size?.height ?? document.body.clientHeight) -

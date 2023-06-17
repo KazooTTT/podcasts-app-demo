62f99521c359ecc8fb9e673c4fa6feb439058d58
KazooTTT <yjwang.echo@gmail.com>
2023-06-17 14:41:20 +0800

    feat:新增我的页面-博客

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function IndexPage() {
   const data = {
@@ -48,7 +49,99 @@ export default function IndexPage() {
           </div>
         </div>
       </div>
-      <div className="list"></div>
+      <div className="tabs mt-4">
+        <Tabs defaultValue="creation" className="container">
+          <TabsList className="w-full bg-contrast">
+            <TabsTrigger
+              value="creation"
+              className="flex-1 text-lg decoration-hightLightMenu decoration-1.5 underline-offset-8 data-[state=active]:underline"
+            >
+              创作
+            </TabsTrigger>
+            <TabsTrigger
+              value="collections"
+              className="flex-1 text-lg decoration-hightLightMenu decoration-1.5 underline-offset-8 data-[state=active]:underline"
+            >
+              收藏
+            </TabsTrigger>
+            <TabsTrigger
+              value="likes"
+              className="flex-1 text-lg decoration-hightLightMenu decoration-1.5 underline-offset-8 data-[state=active]:underline"
+            >
+              赞过
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="creation" className="space-y-3">
+            <div>
+              <div className="mb-2">我创作的博客</div>
+              <div className="podcasts flex items-center space-x-3 overflow-auto">
+                <Image
+                  src={"https://p.ipic.vip/oh91co.png"}
+                  alt="avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-sm"
+                />
+                <Image
+                  src={"https://p.ipic.vip/oh91co.png"}
+                  alt="avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-sm"
+                />
+                <Image
+                  src={"https://p.ipic.vip/oh91co.png"}
+                  alt="avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-sm"
+                />
+                <Image
+                  src={"https://p.ipic.vip/oh91co.png"}
+                  alt="avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-sm"
+                />
+                <Image
+                  src={"https://p.ipic.vip/oh91co.png"}
+                  alt="avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-sm"
+                />
+                <Image
+                  src={"https://p.ipic.vip/oh91co.png"}
+                  alt="avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-sm"
+                />
+                <Image
+                  src={"https://p.ipic.vip/oh91co.png"}
+                  alt="avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-sm"
+                />
+                <Image
+                  src={"https://p.ipic.vip/oh91co.png"}
+                  alt="avatar"
+                  width={64}
+                  height={64}
+                  className="rounded-sm"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="mb-2">我创作的片段</div>
+              <div className="segments"></div>
+            </div>
+          </TabsContent>
+          <TabsContent value="collections">和我创作的片段一样</TabsContent>
+          <TabsContent value="likes">和我创作的片段一样</TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }

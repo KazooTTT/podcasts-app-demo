47866faebd48ecab1eaf0c6b1db04df795d3bc19
KazooTTT <yjwang.echo@gmail.com>
2023-06-20 02:36:37 +0800

    fix:修复my滚动悬浮bug

--- app/my/layout.tsx
+++ app/my/layout.tsx
@@ -10,7 +10,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <div className="relative flex max-h-screen min-h-screen flex-col">
       <SiteHeader />
-      <div className="flex-1 overflow-auto scrollbar-none">{children}</div>
+      <div className="flex flex-1 flex-col overflow-auto scrollbar-none">
+        {children}
+      </div>
       <div className="bottom-menu sticky bottom-0 z-40 w-full">
         <MenuBar></MenuBar>
       </div>

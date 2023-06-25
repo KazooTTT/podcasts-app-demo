d184ce106731dccd7f07020511742b80c6c9d95a
KazooTTT <yjwang.echo@gmail.com>
2023-06-25 23:32:11 +0800

    fix:引入flow layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import { SiteHeader } from "@/components/flow/siteHeader";
-import MenuBar from "@/components/menuBar";
-import Flow from "../components/flow/page";
+import FlowPage from "../components/flow/page";
+import FlowLayout from "../components/flow/layout";
 
 export default function Page() {
   return (
-    <>
-      <div className="relative flex max-h-screen min-h-screen flex-col">
-        <SiteHeader />
-        <div className="items-container flex h-full flex-1 flex-col overflow-auto pb-32 scrollbar-none">
-          <Flow></Flow>
-        </div>
-        <div className="bottom-menu sticky bottom-0 z-40 w-full">
-          <MenuBar></MenuBar>
-        </div>
-      </div>
-    </>
+    <FlowLayout>
+      <FlowPage></FlowPage>
+    </FlowLayout>
   );
 }

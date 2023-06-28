2f2e9509549cd71ea7c3058a4632c79110eebee2
KazooTTT <yjwang.echo@gmail.com>
2023-06-28 23:41:51 +0800

    feat:替换专辑的信息

--- app/album/page.tsx
+++ app/album/page.tsx
@@ -19,208 +19,68 @@ interface AlbumItem {
 
 export default function IndexPage() {
   const info = {
-    title: "被讨厌的勇气",
-    desc: "被讨厌的勇气，并不是要去吸引被讨厌的负向能量，而是，如果这是我生命想绽放出最美的光彩，那么，即使有被讨厌的可能,我都要用自己的双手双脚往那里走去。",
+    title: "组织进化论",
+    desc: "「组织进化论」由飞书与声动活泼联合推出，专注于职场话题和企业管理。我们邀请有趣有料的嘉宾一同分享对于未来工作和管理方式的洞察，希望思维的碰撞可以使大家产生新的思考，让大家更加高效愉悦地工作与生活。",
     tip: "待补充文案",
   };
 
-  const url =
-    "https://kazoottt-1256684243.cos.ap-chengdu.myqcloud.com/202306252109948.jpeg";
+  const cover =
+    "https://kazoottt-1256684243.cos.ap-chengdu.myqcloud.com/%E6%92%AD%E6%94%BE%E9%A1%B5-%E5%A3%B0%E9%9F%B3%26%E4%B8%93%E8%BE%91%E5%A4%B4%E5%83%8F%E3%80%8A%E7%BB%84%E7%BB%87%E8%BF%9B%E5%8C%96%E8%AE%BA%E3%80%8B.jpeg";
 
   const list: AlbumItem[] = [
     {
-      title:
-        "Sit et modi. Corrupti est eos alias similique. Nisi dolorum quasi provident nostrum aut accusamus aspernatur. Qui sapiente maxime quo provident suscipit est porro quisquam. Blanditiis qui doloremque quam consequatur non. Tempora occaecati nostrum et doloribus in quam quaerat. Possimus est nesciunt rem officiis voluptatum. Ducimus quia soluta reprehenderit totam quod. Facilis ut consequuntur qui dolor aspernatur cum aut sed. Exercitationem ut voluptatem ex sunt molestiae temporibus. Praesentium eum corporis quisquam dolor est eaque error.",
-      updateTime: "2023-06-14 15:45:00",
-      duration: 2400,
-      views: 100,
-      recreateCount: 10,
-      cover: url,
+      title: "E38｜为什么晋升总是轮不到你？可能是因为忽视了这项能力",
+      updateTime: "2023-07-06",
+      duration: 3393,
+      views: 120,
+      recreateCount: 80,
+      cover: cover,
       id: "5heajmeeo",
     },
     {
-      title: "Mastering Social Media Marketing",
-      updateTime: "2023-06-14 15:45:00",
-      duration: 2400,
-      views: 100,
-      recreateCount: 10,
-      cover: url,
+      title: "E37｜从大厂到创业公司，如何顺利适应新环境？",
+      updateTime: "2023-06-29",
+      duration: 2881,
+      views: 70,
+      recreateCount: 35,
+      cover: cover,
       id: "5heajmeeo",
     },
     {
-      title: "10 Tips for Successful Blogging",
-      updateTime: "2023-06-15 10:30:00",
-      duration: 1800,
-      views: 100,
-      recreateCount: 22,
-      cover: url,
+      title: "E36｜新晋管理者的避坑指南",
+      updateTime: "2023-06-22",
+      duration: 3963,
+      views: 125,
+      recreateCount: 20,
+      cover: cover,
       id: "ylk68yo00",
     },
     {
-      title: "The Art of Storytelling in Blogging",
-      updateTime: "2023-06-13 09:15:00",
-      duration: 2100,
-      views: 100,
-      recreateCount: 33,
-      cover: url,
+      title: "E35｜年轻人如何“教育”自己的老板？",
+      updateTime: "2023-06-15",
+      duration: 4074,
+      views: 302,
+      recreateCount: 55,
+      cover: cover,
       id: "fxdpxyl0l",
-    },
-    {
-      title: "Mastering Social Media Marketing",
-      updateTime: "2023-06-14 15:45:00",
-      duration: 2400,
-      views: 100,
-      recreateCount: 10,
-      cover: url,
-      id: "5heajmeeo",
-    },
-    {
-      title: "10 Tips for Successful Blogging",
-      updateTime: "2023-06-15 10:30:00",
-      duration: 1800,
-      views: 100,
-      recreateCount: 22,
-      cover: url,
-      id: "ylk68yo00",
-    },
-    {
-      title: "The Art of Storytelling in Blogging",
-      updateTime: "2023-06-13 09:15:00",
-      duration: 2100,
-      views: 100,
-      recreateCount: 33,
-      cover: url,
-      id: "fxdpxyl0l",
-    },
-    {
-      title: "Mastering Social Media Marketing",
-      updateTime: "2023-06-14 15:45:00",
-      duration: 2400,
-      views: 100,
-      recreateCount: 10,
-      cover: url,
-      id: "5heajmeeo",
-    },
-    {
-      title: "The Art of Storytelling in Blogging",
-      updateTime: "2023-06-13 09:15:00",
-      duration: 2100,
-      views: 100,
-      recreateCount: 33,
-      cover: url,
-      id: "fxdpxyl0l",
-    },
-    {
-      title: "The Art of Storytelling in Blogging",
-      updateTime: "2023-06-13 09:15:00",
-      duration: 2100,
-      views: 100,
-      recreateCount: 33,
-      cover: url,
-      id: "fxdpxyl0l",
-    },
-    {
-      title: "The Art of Storytelling in Blogging",
-      updateTime: "2023-06-13 09:15:00",
-      duration: 2100,
-      views: 100,
-      recreateCount: 33,
-      cover: url,
-      id: "fxdpxyl0l",
-    },
-    {
-      title: "Monetizing Your Blog: Strategies and Tips",
-      updateTime: "2023-06-11 18:20:00",
-      duration: 3200,
-      views: 100,
-      recreateCount: 111,
-      cover: url,
-      id: "gjzljl7v3",
-    },
-    {
-      title: "SEO Techniques for Better Blog Visibility",
-      updateTime: "2023-06-12 12:00:00",
-      duration: 2700,
-      views: 100,
-      recreateCount: 44,
-      cover: url,
-      id: "4fje04co1",
-    },
-    {
-      title: "Monetizing Your Blog: Strategies and Tips",
-      updateTime: "2023-06-11 18:20:00",
-      duration: 3200,
-      views: 100,
-      recreateCount: 111,
-      cover: url,
-      id: "gjzljl7v3",
-    },
-    {
-      title: "Mastering Social Media Marketing",
-      updateTime: "2023-06-14 15:45:00",
-      duration: 2400,
-      views: 100,
-      recreateCount: 10,
-      cover: url,
-      id: "5heajmeeo",
-    },
-    {
-      title: "Monetizing Your Blog: Strategies and Tips",
-      updateTime: "2023-06-11 18:20:00",
-      duration: 3200,
-      views: 100,
-      recreateCount: 111,
-      cover: url,
-      id: "gjzljl7v3",
-    },
-    {
-      title: "The Art of Storytelling in Blogging",
-      updateTime: "2023-06-13 09:15:00",
-      duration: 2100,
-      views: 100,
-      recreateCount: 33,
-      cover: url,
-      id: "fxdpxyl0l",
-    },
-    {
-      title: "Monetizing Your Blog: Strategies and Tips",
-      updateTime: "2023-06-11 18:20:00",
-      duration: 3200,
-      views: 100,
-      recreateCount: 111,
-      cover: url,
-      id: "gjzljl7v3",
-    },
-    {
-      title: "Mastering Social Media Marketing",
-      updateTime: "2023-06-14 15:45:00",
-      duration: 2400,
-      views: 100,
-      recreateCount: 10,
-      cover: url,
-      id: "5heajmeeo",
-    },
-    {
-      title: "10 Tips for Successful Blogging",
-      updateTime: "2023-06-15 10:30:00",
-      duration: 1800,
-      views: 100,
-      recreateCount: 22,
-      cover: url,
-      id: "ylk68yo00",
     },
   ];
 
   const [sortType, setSortType] = useState<
-    "duration" | "views" | "recreateCount"
-  >("duration");
+    "default" | "duration" | "views" | "recreateCount"
+  >("default");
+
+  const sortedList =
+    sortType === "default"
+      ? list
+      : list.sort((a, b) => a?.[sortType] - b?.[sortType]);
 
   return (
     <div className="container relative flex w-full flex-col items-center justify-center">
       <div className="sticky top-0 z-10 w-full bg-background py-4">
         <div className="info flex w-full">
           <Image
-            src={url}
+            src={cover}
             alt="cover img"
             width={144}
             height={144}
@@ -244,7 +104,7 @@ export default function IndexPage() {
             className="duration h-8 w-1/3 space-x-0.5 rounded-sm px-0"
             variant={sortType === "duration" ? undefined : "secondary"}
             onClick={() => {
-              setSortType("duration");
+              setSortType(sortType === "duration" ? "default" : "duration");
             }}
           >
             <ArrowUpDown size={20} />
@@ -254,7 +114,7 @@ export default function IndexPage() {
             className="views h-8 w-1/3 space-x-0.5 rounded-sm px-0"
             variant={sortType === "views" ? undefined : "secondary"}
             onClick={() => {
-              setSortType("views");
+              setSortType(sortType === "views" ? "default" : "views");
             }}
           >
             <ArrowUpDown size={20} />
@@ -264,7 +124,9 @@ export default function IndexPage() {
             className="recreateCount h-8 w-1/3 space-x-0.5 rounded-sm px-0"
             variant={sortType === "recreateCount" ? undefined : "secondary"}
             onClick={() => {
-              setSortType("recreateCount");
+              setSortType(
+                sortType === "recreateCount" ? "default" : "recreateCount"
+              );
             }}
           >
             <ArrowUpDown size={20} />
@@ -273,42 +135,36 @@ export default function IndexPage() {
         </div>
       </div>
       <div className="flex-1 space-y-4 overflow-hidden py-4">
-        {list
-          .sort((a, b) => a?.[sortType] - b?.[sortType])
-          .map((item, index) => (
-            <div
-              className="item flex items-center"
-              key={[item.id, index].join("-")}
-            >
-              <div className="mr-2">
-                <Image
-                  src={item.cover}
-                  alt="cover img"
-                  width={72}
-                  height={72}
-                  className="rounded-md"
-                />
-              </div>
-              <div className="flex flex-1 flex-col justify-center space-y-2">
-                <div className="line-clamp-1 text-base">{item.title}</div>
-                <div className="flex items-center space-x-1 text-xs text-muted-foreground">
-                  <div>{formatTime(item.updateTime)}</div>
-                  <div>{formatDuration(item.duration)}</div>
-                  <div className="flex items-center">
-                    <PlayCircle
-                      strokeWidth={1.5}
-                      size={16}
-                      className="mr-0.5"
-                    />
-                    片段创作{item.recreateCount}次
-                  </div>
+        {sortedList.map((item, index) => (
+          <div
+            className="item flex items-center"
+            key={[item.id, index].join("-")}
+          >
+            <div className="mr-2">
+              <Image
+                src={item.cover}
+                alt="cover img"
+                width={72}
+                height={72}
+                className="rounded-md"
+              />
+            </div>
+            <div className="flex flex-1 flex-col justify-center space-y-2">
+              <div className="line-clamp-1 text-base">{item.title}</div>
+              <div className="flex items-center space-x-3 text-xs text-muted-foreground">
+                <div>{formatTime(item.updateTime)}</div>
+                <div>{formatDuration(item.duration)}</div>
+                <div className="flex items-center">
+                  <PlayCircle strokeWidth={1.5} size={16} className="mr-0.5" />
+                  片段创作{item.recreateCount}次
                 </div>
               </div>
-              <div className="text-muted-foreground">
-                <Info size={24} strokeWidth={1.5} />
-              </div>
             </div>
-          ))}
+            {/* <div className="text-muted-foreground">
+                <Info size={24} strokeWidth={1.5} />
+              </div> */}
+          </div>
+        ))}
       </div>
     </div>
   );

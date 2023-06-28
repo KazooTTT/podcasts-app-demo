e642d6392da8858ef6fb8e7cbbdeb3c93980fe61
KazooTTT <yjwang.echo@gmail.com>
2023-06-28 21:27:54 +0800

    feat:修改html的title

--- config/site.ts
+++ config/site.ts
@@ -1,21 +1,8 @@
+import { Podcast } from 'lucide-react';
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Podcast App",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
-  mainNav: [
-    {
-      title: "播放",
-      href: "/play",
-    },
-    {
-      title: "分段",
-      href: "/",
-    },
-    {
-      title: "文稿",
-      href: "/content",
-    },
-  ],
 };

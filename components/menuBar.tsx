b6abd3aba1c2e19b6e9659b24b6e1d0c7f9eea65
KazooTTT <yjwang.echo@gmail.com>
2023-06-29 21:17:38 +0800

    feat:上传按钮改成播放

--- components/menuBar.tsx
+++ components/menuBar.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { usePathname, useRouter } from "next/navigation";
-import { Heart, Home, Mail, PlusSquare, Upload, User } from "lucide-react";
+import { Heart, Home, Mail, PlayCircle, User } from "lucide-react";
 
 import { Menubar, MenubarMenu, MenubarTrigger } from "./ui/menubar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 const MenuBar = () => {
   // get pathname in nextjs
@@ -53,26 +56,21 @@ const MenuBar = () => {
           </div>
         </div>
       </MenubarMenu>
-      <MenubarMenu>
-        <div className="text-center">
-          <MenubarTrigger
-            className="mb-0.5 flex w-12 flex-col items-center data-[state=open]:bg-hightLightMenu"
-            data-state={isActive("/upload") ? "open" : undefined}
-            onClick={() => {
-              router.push("/upload");
-            }}
-          >
-            <div>
-              <PlusSquare alt="upload"></PlusSquare>
-            </div>
-          </MenubarTrigger>
-          <div
-            className={isActive("/upload") ? "text-hightLightMenu" : "white"}
-          >
-            创作
-          </div>
+      <div className="flex w-12 flex-col items-center">
+        <div className="relative h-12 w-12">
+          <Image
+            src="https://kazoottt-1256684243.cos.ap-chengdu.myqcloud.com/%E6%92%AD%E6%94%BE%E9%A1%B5-%E5%A3%B0%E9%9F%B3%26%E4%B8%93%E8%BE%91%E5%A4%B4%E5%83%8F%E3%80%8A%E7%BB%84%E7%BB%87%E8%BF%9B%E5%8C%96%E8%AE%BA%E3%80%8B.jpeg"
+            alt="album"
+            className="rounded-full opacity-50"
+            height={48}
+            width={48}
+          />
+          <PlayCircle
+            className="absolute top-0 h-12 w-12"
+            strokeWidth={0.5}
+          ></PlayCircle>
         </div>
-      </MenubarMenu>
+      </div>
       <MenubarMenu>
         <div className="text-center">
           <MenubarTrigger

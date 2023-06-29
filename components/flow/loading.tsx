d237f053917d653e2d17d9c0b035e7041c6a5fa2
KazooTTT <yjwang.echo@gmail.com>
2023-06-29 20:32:30 +0800

    feat:骨架屏宽高度设置

--- components/flow/loading.tsx
+++ components/flow/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
     <div className="flex h-screen w-screen flex-col items-center space-x-4 space-y-2">
-      <Skeleton className="h-32 w-48 rounded-md" />
+      <Skeleton className="h-48 w-48 rounded-md" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-[250px]" />
         <Skeleton className="h-4 w-[200px]" />
